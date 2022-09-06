310e658518c03b028e403348c00b039662f7cf63
sudhanshuksingh124197 <112933789+sudhanshuksingh124197@users.noreply.github.com>
2022-09-06 13:40:40 +0530

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,10 @@
+<?php $name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "sudhanshu124197@gmail.com";
+$subject = "$name Contacted You Using your website RN CABLE";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!" . " -" . "<a href='index.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";;
+?>
